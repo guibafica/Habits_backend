51d881869c6a92e3ab09596b2ac414eca1dca7c0
Guilherme Bafica <gb.engcomp@hotmail.com>
2023-01-24 16:59:44 -0300

    Create first table

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('oi')
+import Fastify from 'fastify';
+
+const app = Fastify();
+
+app.get('/', () => {
+  return 'Hello world!'
+});
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('🚀 HTTP server listening on port 3333 🚀')
+});
