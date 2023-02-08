999ec6b6eb73e0aeccbc95832d1b2425f91bce61
Guilherme Bafica <gb.engcomp@hotmail.com>
2023-02-07 21:07:04 -0300

    Create complete habit route

--- src/routes.ts
+++ src/routes.ts
@@ -73,4 +73,52 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits,
     }
   });
+
+  app.patch('/habits/:id/toggle', async (req) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(req.params);
+
+    const todayDate = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: todayDate,
+      }
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: todayDate,
+        }
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      }
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        }
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      });
+    }
+  });
 }
